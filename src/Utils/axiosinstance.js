74e8284467611e177782d8d32acd9d10e267741b
Ali-dotcom98 <alishah1234584.as@gmail.com>
2025-09-22 18:35:28 +0500

    Axios Instances

--- src/Utils/axiosinstance.js
+++ src/Utils/axiosinstance.js
@@ -24,26 +24,7 @@ axiosInstance.interceptors.request.use(
     (error) => Promise.reject(error)
 );
 
-// Response Interceptor: Handles global error logic
-axiosInstance.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        if (error.response) {
-            const status = error.response.status;
 
-            if (status === 401) {
-                console.warn("Unauthorized (401). Redirecting to login...");
-                window.location.href = "/";
-            } else if (status === 500) {
-                console.error("Server error (500). Please try again later.");
-            }
-        } else if (error.code === "ECONNABORTED") {
-            console.error("Request timeout. Please try again.");
-        }
-
-        return Promise.reject(error);
-    }
-);
 
 export default axiosInstance;
 
